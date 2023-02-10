dc9d48a4fb7362ea2bf95eed32e0df783e4dcc82
realbits-lab <dev.realbits@gmail.com>
2023-02-10 16:14:26 +0900

    Add console log for process.env.

--- pages/api/post.js
+++ pages/api/post.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
   ) {
     console.error("discordBotToken is different.");
     console.error("discordBotToken: ", discordBotToken);
+    console.error("process.env: ", process.env);
     console.error(
       "process.env.NEXT_PUBLIC_DISCORD_BOT_TOKEN: ",
       process.env.NEXT_PUBLIC_DISCORD_BOT_TOKEN
