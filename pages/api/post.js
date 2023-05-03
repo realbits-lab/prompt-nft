f0ff1678a068aad5c7749380be2c3c92709a1760
realbits-lab <dev.realbits@gmail.com>
2023-05-03 16:57:28 +0900

    Add channel id and bot token to .env for moim connect.

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,25 +1,78 @@
 import { PrismaClient } from "@prisma/client";
+import axios from "axios";
 
 const prisma = new PrismaClient();
+
+function truncate(str, n) {
+  return str.length > n ? str.slice(0, n - 1) + "..." : str;
+}
+
+async function uploadMoimPost({ title, imageUrl }) {
+  const MOIM_BOT_TOKEN = process.env.MOIM_BOT_TOKEN;
+  const MOIM_DOMAIN = process.env.MOIM_DOMAIN;
+  const MOIM_CHANNEL_ID = process.env.MOIM_CHANNEL_ID;
+
+  //* Post thread for content and previewBottom.
+  //* https://vingle.atlassian.net/wiki/spaces/MP/pages/364904472/Blockit
+  const postThreadData = {
+    thread: {
+      title: title,
+      content: [
+        {
+          type: "image",
+          width: 512,
+          height: 512,
+          src: imageUrl,
+        },
+      ],
+      previewBottom: [
+        {
+          type: "image",
+          width: 512,
+          height: 512,
+          src: imageUrl,
+        },
+      ],
+    },
+  };
+
+  let postResponse;
+  try {
+    postResponse = await axios({
+      method: "post",
+      url: `${MOIM_DOMAIN}/api/forums/${MOIM_CHANNEL_ID}/threads`,
+      headers: {
+        authorization: `Bearer ${MOIM_BOT_TOKEN}`,
+      },
+      data: postThreadData,
+      responseType: "json",
+    });
+  } catch (error) {
+    throw error;
+  }
+  // console.log("postResponse: ", postResponse);
+
+  return postResponse;
+}
 
 export default async function handler(req, res) {
   console.log("call /api/post");
 
-  // Check method error.
+  //* Check method error.
   if (req.method !== "POST") {
     console.log("req.method: ", req.method);
     res.status(500).json({ error: "Invalid method. Support only POST." });
     return;
   }
 
-  // POST /api/post
-  // Required fields in body: prompt, imageUrl, discordBotToken
+  //* Required fields in body: prompt, imageUrl, discordBotToken
   const { prompt, negativePrompt, imageUrl, discordBotToken } = req.body;
   console.log("prompt: ", prompt);
   console.log("negativePrompt: ", negativePrompt);
   console.log("imageUrl: ", imageUrl);
   console.log("discordBotToken: ", discordBotToken);
 
+  //* Check discord bot token match.
   if (
     !discordBotToken ||
     discordBotToken !== process.env.NEXT_PUBLIC_DISCORD_BOT_TOKEN
@@ -34,19 +87,45 @@ export default async function handler(req, res) {
     return;
   }
 
-  const result = await prisma.post.create({
-    data: {
-      prompt: prompt,
-      negativePrompt: negativePrompt,
-      imageUrl: imageUrl,
-    },
-  });
-  console.log("prisma.post.create result: ", result);
+  //* Insert image data.
+  let resultPrismaPostCreate;
+  try {
+    resultPrismaPostCreate = await prisma.post.create({
+      data: {
+        prompt: prompt,
+        negativePrompt: negativePrompt,
+        imageUrl: imageUrl,
+      },
+    });
+    // console.log("resultPrismaPostCreate: ", resultPrismaPostCreate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "prisma.post.create failed." });
+    return;
+  }
 
-  if (result) {
+  //* Write miom post.
+  let resultUploadMoimPost;
+  try {
+    resultUploadMoimPost = await uploadMoimPost({
+      imageUrl: imageUrl,
+      title: truncate(prompt, 10),
+    });
+    // console.log(
+    //   "resultUploadMoimPost.data.data: ",
+    //   resultUploadMoimPost.data.data
+    // );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "uploadMoimPost failed." });
+    return;
+  }
+
+  //* Return database insert result.
+  if (resultPrismaPostCreate) {
     res.status(200).json({ data: "ok" });
   } else {
-    console.error("data creation failed. result: ", result);
+    console.error("resultPrismaPostCreate: ", resultPrismaPostCreate);
     res.status(500).json({ message: "data creation failed." });
   }
 }
