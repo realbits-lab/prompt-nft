b040b278cc4e2a3c0f0e29a69016dcf6bb405f95
realbits-lab <dev.realbits@gmail.com>
2023-06-17 14:23:50 +0900

    Remove console logs.

--- components/CardRent.js
+++ components/CardRent.js
@@ -120,7 +120,7 @@ function CardRent({
         )}
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            token id: {nftData.tokenId.toNumber()}
+            token id: {Number(nftData.tokenId)}
           </Typography>
           <Typography
             sx={{ fontSize: 14 }}
@@ -144,7 +144,7 @@ function CardRent({
             gutterBottom
             component="div"
           >
-            rent fee: {nftData.rentFee / Math.pow(10, 18)} matic
+            rent fee: {Number(nftData.rentFee) / Math.pow(10, 18)} matic
           </Typography>
         </CardContent>
         <CardActions>
