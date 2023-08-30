34fd9e19c6f5cebb521e4528106359f34ba48fe3
realbits-lab <dev.realbits@gmail.com>
2023-08-30 10:57:24 +0900

    Use original SD and reverse nft list.

--- pages/api/draw.js
+++ pages/api/draw.js
@@ -23,8 +23,8 @@ async function handler(req, res) {
   }
 
   // Stable diffusion api url.
-  // const TEXT2IMG_API_URL = "https://stablediffusionapi.com/api/v3/text2img";
-  const TEXT2IMG_API_URL = "https://stablediffusionapi.com/api/v4/dreambooth";
+  const TEXT2IMG_API_URL = "https://stablediffusionapi.com/api/v3/text2img";
+  // const TEXT2IMG_API_URL = "https://stablediffusionapi.com/api/v4/dreambooth";
   const STABLE_DIFFUSION_API_KEY =
     process.env.NEXT_PUBLIC_STABLE_DIFFUSION_API_KEY;
   const WIDTH = "1024";
@@ -42,33 +42,48 @@ async function handler(req, res) {
 
   //* Stable diffusion api option.
   const jsonData = {
-    key: STABLE_DIFFUSION_API_KEY,
-    model_id: "sdxl",
+    key: process.env.NEXT_PUBLIC_STABLE_DIFFUSION_API_KEY,
     prompt: prompt,
     negative_prompt: negativePrompt,
-    width: WIDTH,
-    height: HEIGHT,
-    samples: SAMPLES,
-    num_inference_steps: NUM_INFERENCE_STEPS,
+    width: "512",
+    height: "512",
+    samples: "1",
+    num_inference_steps: "20",
     safety_checker: "yes",
     enhance_prompt: "no",
     seed: null,
-    guidance_scale: GUIDANCE_SCALE,
-    multi_lingual: "no",
-    panorama: "no",
-    self_attention: "no",
-    upscale: "no",
-    embeddings_model: null,
-    lora_model: null,
-    tomesd: "yes",
-    use_karras_sigmas: "yes",
-    vae: null,
-    lora_strength: null,
-    scheduler: "DPMSolverMultistepScheduler",
-    clip_skip: CLIP_SKIP,
+    guidance_scale: 7.5,
     webhook: null,
     track_id: null,
   };
+  // const jsonData = {
+  //   key: STABLE_DIFFUSION_API_KEY,
+  //   model_id: "sdxl",
+  //   prompt: prompt,
+  //   negative_prompt: negativePrompt,
+  //   width: WIDTH,
+  //   height: HEIGHT,
+  //   samples: SAMPLES,
+  //   num_inference_steps: NUM_INFERENCE_STEPS,
+  //   safety_checker: "yes",
+  //   enhance_prompt: "no",
+  //   seed: null,
+  //   guidance_scale: GUIDANCE_SCALE,
+  //   multi_lingual: "no",
+  //   panorama: "no",
+  //   self_attention: "no",
+  //   upscale: "no",
+  //   embeddings_model: null,
+  //   lora_model: null,
+  //   tomesd: "yes",
+  //   use_karras_sigmas: "yes",
+  //   vae: null,
+  //   lora_strength: null,
+  //   scheduler: "DPMSolverMultistepScheduler",
+  //   clip_skip: CLIP_SKIP,
+  //   webhook: null,
+  //   track_id: null,
+  // };
 
   //* Fetch image.
   let myHeaders = new Headers();
