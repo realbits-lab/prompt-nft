79c3d0401d59e1862258ff32e40f6d9c146592f7
realbits-lab <dev.realbits@gmail.com>
2023-09-05 09:38:10 +0900

    Apply typescript to WalletProfile.

--- components/WalletProfile.tsx
+++ components/WalletProfile.tsx
@@ -6,23 +6,24 @@ import { shortenAddress } from "@/lib/util";
 export default function WalletProfile() {
   const CARD_MARGIN_TOP = "60px";
 
-  //*---------------------------------------------------------------------------
-  //* Wagmi.
-  //*---------------------------------------------------------------------------
+  //* Wagmi hooks.
   const { address, isConnected, isConnecting } = useAccount();
   const { chain } = useNetwork();
 
   return (
     <>
       <Box display="flex" flexDirection="row" sx={{ mt: CARD_MARGIN_TOP }}>
-        <Button>{shortenAddress({ address })}</Button>
-        <Button>{chain?.network}</Button>
-        <Button>
+        <Button variant="outlined">
+          Address : {shortenAddress({ address })}
+        </Button>
+        <Button variant="outlined">Network : {chain?.network || "n/a"}</Button>
+        <Button variant="outlined">
+          Status :{" "}
           {isConnected
             ? "connected"
             : isConnecting
             ? "connecting"
-            : "n/a connection"}
+            : "not connected"}
         </Button>
       </Box>
     </>
