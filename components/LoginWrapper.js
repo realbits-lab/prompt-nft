aa3cc9d36fbb05dedc4821e7bc2b2465384ecb20
realbits-lab <dev.realbits@gmail.com>
2023-10-31 20:57:11 +0900

    Add for mobile device case.

--- components/LoginWrapper.js
+++ components/LoginWrapper.js
@@ -2,6 +2,13 @@ import Box from "@mui/material/Box";
 import useUser from "@/lib/useUser";
 import User from "@/components/User";
 import CircularProgress from "@mui/material/CircularProgress";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
+import { Typography } from "@mui/material";
 
 export default function LoginWrapper({ children }) {
   // console.log("call LoginWrapper()");
@@ -16,12 +23,33 @@ export default function LoginWrapper({ children }) {
   //*---------------------------------------------------------------------------
   //* Constant variables.
   //*---------------------------------------------------------------------------
-  const CARD_MARGIN_TOP = "80px";
+  const CARD_MARGIN_TOP = "60px";
   const CARD_MAX_WIDTH = 420;
   const CARD_MIN_WIDTH = 375;
 
-  if (user?.isLoggedIn === true) {
-    return <>{children}</>;
+  if (isLoading === false || user?.isLoggedIn === true) {
+    return (
+      <>
+        <BrowserView>{children}</BrowserView>
+        <MobileView>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            sx={{
+              mt: CARD_MARGIN_TOP,
+              mx: CARD_MARGIN_TOP,
+            }}
+          >
+            <Typography variant="h6" color="primary">
+              No support for mobile device now. Sorry for that. Please use
+              desktop chrome browser for this fictures service.
+            </Typography>
+          </Box>
+        </MobileView>
+      </>
+    );
   }
 
   return (
