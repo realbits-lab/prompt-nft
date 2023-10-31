816addb2dc66927498f1b4a58001ab24169aee84
realbits-lab <dev.realbits@gmail.com>
2023-10-31 10:40:11 +0900

    Remove list directory and change link url for that.

--- pages/list/[[...index]].js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import dynamic from "next/dynamic";
-const ListPage = dynamic(() => import("../../components/ListPage"), {
-  ssr: false,
-});
-
-export default function handler() {
-  return <ListPage />;
-}
