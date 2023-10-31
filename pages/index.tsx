b01d373da41e1d1d532f829e9832f4114d0825c3
realbits-lab <dev.realbits@gmail.com>
2023-10-31 09:39:08 +0900

    Remove IntroPage component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import dynamic from "next/dynamic";
-import IntroPage from "./intro.page";
+
 const ListPage = dynamic(() => import("../components/ListPage"), {
   ssr: false,
 });
 
 export default function Home() {
-  return <IntroPage />;
+  return <ListPage />;
 }
